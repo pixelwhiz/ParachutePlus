353b3f28ee655c7e4fc3fd7ca3673a8f92bb3882
pixelwhiz <daffa01futn@gmail.com>
2024-07-29 15:49:25 +0700

    added Parachute.php

---
+++ src/items/Parachute.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace pixelwhiz\parachute\items;
+
+
+use pocketmine\item\Egg;
+use pocketmine\item\ItemIdentifier;
+use pocketmine\item\ItemTypeIds;
+
+class Parachute extends Egg {
+
+    public function __construct()
+    {
+        parent::__construct(new ItemIdentifier(ItemTypeIds::EGG), "Parachute");
+        $this->setCustomName("Parachute");
+    }
+
+    public static function getItem(): Item {
+        $item = VanillaItems::EGG();
+        $item->setCustomName("Parachute");
+        return $item;
+    }
+
+    public static function addItem(Player $player): void {
+        $player->getInventory()->addItem(self::getItem());
+    }
+
+    public static function getItemName(): string {
+        return "Parachute";
+    }
+
+    public static function getItemId(): int {
+        return ItemTypeIds::EGG;
+    }
+
+}
