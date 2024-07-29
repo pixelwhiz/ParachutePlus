a2c8281afa6b5cf6b9cbc64f81e0286aa03e4f0c
pixelwhiz <daffa01futn@gmail.com>
2024-07-29 15:46:04 +0700

    added ParachutesCommands.php

---
+++ src/commands/ParachutesCommands.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace pixelwhiz\parachute\commands;
+
+use pixelwhiz\parachute\items\Parachute;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class ParachutesCommands extends Command {
+
+    public function __construct()
+    {
+        parent::__construct("parachute", "Parachutes main commands", "/parachute give <player name>", ["pc"]);
+        $this->setPermission("parachutes.cmd");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if (!$sender->hasPermission("parachutes.cmd")) {
+            $sender->sendMessage(TextFormat::RED . "You don't have permission to use this command.");
+            return false;
+        }
+
+        if (count($args) < 1) {
+            $sender->sendMessage(TextFormat::GRAY . "Usage: ". TextFormat::RED . $this->getUsage());
+            return false;
+        }
+
+        $playerName = $args[1];
+        $player = Server::getInstance()->getPlayerExact($playerName);
+
+        if (!$player instanceof Player) {
+            $sender->sendMessage(TextFormat::RED . "Player not found!");
+            return false;
+        }
+
+        $player->getInventory()->addItem(new Parachute());
+        $sender->sendMessage(TextFormat::GREEN . "Gave Parachute(s) to {$player->getName()}");
+    }
+}
